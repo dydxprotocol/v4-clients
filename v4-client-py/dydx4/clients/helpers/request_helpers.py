9099e539b48f90ad240cb87d6e11ca7debb08550
Christopher-Li <Christopher-Li@users.noreply.github.com>
2023-08-31 13:04:37 -0400

    Fix query string to cast bool to lowercase string (#20)
    * Fix request helper
    
    * nits
    
    * nit
    
    * update version

--- v4-client-py/dydx4/clients/helpers/request_helpers.py
+++ v4-client-py/dydx4/clients/helpers/request_helpers.py
@@ -13,7 +13,8 @@ def generate_query_path(url, params):
         return url
 
     paramsString = '&'.join('{key}={value}'.format(
-        key=x[0], value=x[1]) for x in entries if x[1] is not None)
+      key=x[0],
+      value=str(x[1]).lower() if isinstance(x[1], bool) else x[1]) for x in entries if x[1] is not None)
     if paramsString:
         return url + '?' + paramsString
 
