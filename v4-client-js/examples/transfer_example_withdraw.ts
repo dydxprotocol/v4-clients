fe6e853aa9714db491a4feca25f591ab284d9fc9
Will Liu <will@dydx.exchange>
2023-12-07 10:21:25 -0500

    latest

--- v4-client-js/examples/transfer_example_withdraw.ts
+++ v4-client-js/examples/transfer_example_withdraw.ts
@@ -5,15 +5,15 @@ import { Network } from '../src/clients/constants';
 import LocalWallet from '../src/clients/modules/local-wallet';
 import { SubaccountInfo } from '../src/clients/subaccount';
 import { ValidatorClient } from '../src/clients/validator-client';
-import { DYDX_TEST_MNEMONIC } from './constants';
+import { DYDX_LOCAL_MNEMONIC, DYDX_TEST_MNEMONIC } from './constants';
 
 // TODO: Test after testnet deploy latest transfer contracts.
 
 async function test(): Promise<void> {
-  const wallet = await LocalWallet.fromMnemonic(DYDX_TEST_MNEMONIC, BECH32_PREFIX);
+  const wallet = await LocalWallet.fromMnemonic(DYDX_LOCAL_MNEMONIC, BECH32_PREFIX);
   console.log(wallet);
 
-  const client = await ValidatorClient.connect(Network.testnet().validatorConfig);
+  const client = await ValidatorClient.connect(Network.local().validatorConfig);
   console.log('**Client**');
   console.log(client);
 
@@ -22,7 +22,7 @@ async function test(): Promise<void> {
   const tx = await client.post.withdraw(
     subaccount,
     0,
-    new Long(1_00_000_000),
+    new Long(50_000_000),
   );
   console.log('**Withdraw Tx**');
   console.log(tx);
