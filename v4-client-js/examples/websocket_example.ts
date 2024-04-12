cd25b7f79d82d82e017a07dec944d9ae018ab8bd
Will Liu <will@dydx.exchange>
2024-04-12 14:33:05 -0400

    more scripts

--- v4-client-js/examples/websocket_example.ts
+++ v4-client-js/examples/websocket_example.ts
@@ -12,7 +12,10 @@ function delay(ms: number) {
 }
 
 async function test(): Promise<void> {
-  const orderIds: Record<string, string[]> = {};
+  const clientIds = new Set();
+  const sawRemoveBeforePlace = new Set();
+  const orderIdToTimestamp: Record<string, string> = {};
+  let orderCount = 0;
   // now in format yyyy-mm-dd HH:MM:SS & utc timezone
   const startTimestamp: string = formatUTCDateWithMoment(new Date());
   const mySocket = new SocketClient(
@@ -33,12 +36,22 @@ async function test(): Promise<void> {
           }
           // console.log(JSON.stringify(data));
           if (data.type === IncomingMessageTypes.CHANNEL_DATA && data.contents.orders) {
+            // console.log(`orders: ${JSON.stringify(data.contents.orders)}`);
             for (const order of data.contents.orders) {
-              // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
-              if (!orderIds[order.ticker]) {
-                orderIds[order.ticker] = [];
+              if (order.ticker !== 'AEVO-USD') {
+                continue;
               }
-              orderIds[order.ticker].push(order.id);
+              orderCount++;
+              if (order.clientId in clientIds) {  // seen before
+                if (!orderIdToTimestamp[order.clientId]) {
+                  orderIdToTimestamp[order.clientId] = new Date().getTime().toString();
+                }
+              } else {  // first time seen
+                if (order.status === 'BEST_EFFORT_CANCELED') {
+                  sawRemoveBeforePlace.add(order.clientId);
+                }
+              }
+              clientIds.add(order.clientId);
             }
           }
         } catch (e) {
@@ -48,9 +61,18 @@ async function test(): Promise<void> {
     },
   );
   mySocket.connect();
-  await delay(5 * 1000); // 1 minute
+  await delay(60 * 5 * 1000); // 5 minutes
   mySocket.terminate();
-  console.log('Order IDs:', orderIds);
+
+  console.log('Order count:', orderCount);
+  console.log('Order timestamps: ');
+  for (const [orderId, timestamp] of Object.entries(orderIdToTimestamp)) {
+    console.log(`${orderId}: ${timestamp}`);
+  }
+  console.log(`Saw remove before place: ${sawRemoveBeforePlace.size}`);
+  for (const orderId of sawRemoveBeforePlace) {
+    console.log(orderId);
+  }
   const endTimestamp: string = formatUTCDateWithMoment(new Date());
   console.log('Start timestamp:', startTimestamp);
   console.log('End timestamp:', endTimestamp);
