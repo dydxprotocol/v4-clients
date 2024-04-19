ebe8acd85e1a73d2eeac7046bb1c06984f7a0818
samtin0x <40127309+samtin0x@users.noreply.github.com>
2024-04-19 11:35:36 +0200

    Add tests for account rest endpoints

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,32 @@
 import pytest
 
 from dydx_v4_client import ValidatorClient
+from dydx_v4_client.clients.constants import (
+    IndexerApiHost,
+    IndexerConfig,
+    IndexerWSHost,
+)
+from dydx_v4_client.clients.indexer_client import IndexerClient
 from dydx_v4_client.network import TESTNET
 
 pytest_plugins = ("pytest_asyncio",)
+
+DYDX_TEST_ADDRESS = "dydx14zzueazeh0hj67cghhf9jypslcf9sh2n5k6art"
+DYDX_TEST_PRIVATE_KEY = (
+    "e92a6595c934c991d3b3e987ea9b3125bf61a076deab3a9cb519787b7b3e8d77"
+)
+DYDX_TEST_MNEMONIC = (
+    "mirror actor skill push coach wait confirm orchard lunch mobile athlete gossip awake "
+    "miracle matter bus reopen team ladder lazy list timber render wait"
+)
+
+
+@pytest.fixture
+def indexer_client():
+    config = IndexerConfig(
+        rest_endpoint=IndexerApiHost.TESTNET, websocket_endpoint=IndexerWSHost.TESTNET
+    )
+    return IndexerClient(config)
 
 
 @pytest.fixture
