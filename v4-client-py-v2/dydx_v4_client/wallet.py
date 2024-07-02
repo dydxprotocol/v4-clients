323008986b66cb79636904594ab8e9479f8e1dc1
samtin0x <40127309+samtin0x@users.noreply.github.com>
2024-07-02 23:16:35 +0200

    Add sequence manager

--- v4-client-py-v2/dydx_v4_client/wallet.py
+++ v4-client-py-v2/dydx_v4_client/wallet.py
@@ -3,7 +3,9 @@ from dataclasses import dataclass
 from functools import partial
 from typing import TYPE_CHECKING
 
+import bech32
 import ecdsa
+from Crypto.Hash import RIPEMD160
 from bip_utils import Bip39SeedGenerator, Bip44, Bip44Coins
 from v4_proto.cosmos.crypto.secp256k1.keys_pb2 import PubKey
 
@@ -45,3 +47,10 @@ class Wallet:
     @property
     def public_key(self) -> PubKey:
         return PubKey(key=self.key.get_verifying_key().to_string("compressed"))
+
+    @property
+    def address(self) -> str:
+        public_key_bytes = self.public_key.key
+        sha256_hash = hashlib.sha256(public_key_bytes).digest()
+        ripemd160_hash = RIPEMD160.new(sha256_hash).digest()
+        return bech32.bech32_encode("dydx", bech32.convertbits(ripemd160_hash, 8, 5))
