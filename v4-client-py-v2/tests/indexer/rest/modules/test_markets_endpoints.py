5cd6d4e56efd2e7755721017942bef3f80799626
sirEven <sir_even@icloud.com>
2024-07-31 12:37:52 +0200

    Add two identical tests to illustrate differing behaviour.

--- v4-client-py-v2/tests/indexer/rest/modules/test_markets_endpoints.py
+++ v4-client-py-v2/tests/indexer/rest/modules/test_markets_endpoints.py
@@ -3,6 +3,7 @@ import pytest
 from tests.conftest import retry_on_forbidden
 
 MARKET_BTC_USD: str = "BTC-USD"
+MARKET_ETH_USD: str = "ETH-USD"
 
 
 @pytest.mark.asyncio
@@ -53,6 +54,40 @@ async def test_btc_candles(indexer_rest_client):
     )
     candles = response["candles"]
     assert candles is not None
+
+
+# Request: '/v4/candles/perpetualMarkets/ETH-USD?resolution=1MIN&fromISO=2024-04-10T00:00:00.000Z&toISO=2024-04-10T01:00:00.000Z'
+# Result: 60 1min candles ('00 - '59 representing first hour of april 10.)
+@pytest.mark.asyncio
+@retry_on_forbidden(max_retries=3, delay=1)
+async def test_eth_candles_by_range_newer_than_april_2024(indexer_rest_client):
+    response = await indexer_rest_client.markets.get_perpetual_market_candles(
+        MARKET_ETH_USD,
+        "1MIN",
+        "2024-04-10T00:00:00.000Z",
+        "2024-04-10T01:00:00.000Z",
+    )
+    candles = response["candles"]
+    # breakpoint()
+    assert candles is not None
+    assert len(candles) > 0
+
+
+# Request: '/v4/candles/perpetualMarkets/ETH-USD?resolution=1MIN&fromISO=2024-05-10T00:00:00.000Z&toISO=2024-05-10T01:00:00.000Z'
+# Result: No candles at all (it should be 60 1min candles ('00 - '59 representing first hour of may 10.))
+@pytest.mark.asyncio
+@retry_on_forbidden(max_retries=3, delay=1)
+async def test_eth_candles_by_range_newer_than_may_2024(indexer_rest_client):
+    response = await indexer_rest_client.markets.get_perpetual_market_candles(
+        MARKET_ETH_USD,
+        "1MIN",
+        "2024-05-10T00:00:00.000Z",
+        "2024-05-10T01:00:00.000Z",
+    )
+    candles = response["candles"]
+    # breakpoint()
+    assert candles is not None  # Note: Succeeds on empty candles
+    assert len(candles) > 0
 
 
 @pytest.mark.asyncio
