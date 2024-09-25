187f59f008716c1a22a9a162017e89bc0288f3b8
aforaleka <aforaleka@gmail.com>
2024-09-25 13:54:24 -0400

    build and bump

--- v4-client-js/src/clients/validator-client.ts
+++ v4-client-js/src/clients/validator-client.ts
@@ -69,6 +69,16 @@ export class ValidatorClient {
     this._post.setSelectedGasDenom(gasDenom);
   }
 
+
+  /**
+   * @description populate account number cache in the Post module for performance.
+   */
+  populateAccountNumberCache(address: string): Promise<void> {
+    if (!this._post) throw new Error('Post module not initialized');
+
+    return this._post.populateAccountNumberCache(address);
+  }
+
   private async initialize(): Promise<void> {
     const tendermint37Client: Tendermint37Client = await Tendermint37Client.connect(
       this.config.restEndpoint,
