a6e8b910bb6af14e383ee010d16f1320e2a78f8d
Tyler <tyler@dydx.exchange>
2025-06-02 19:03:25 -0400

    fix

--- v4-client-js/jest.globalSetup.js
+++
@@ -1,7 +0,0 @@
-// This function runs once before all tests.
-// eslint-disable-next-line
-export default async () => {
-  // eslint-disable-next-line
-  const { config } = await import('dotenv-flow');
-  config();
-};
