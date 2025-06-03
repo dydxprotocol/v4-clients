3b33a3023bedf715f1ddfc16a5a78ed2dd4c44f7
tyleroooo <tyler@dydx.exchange>
2025-06-03 13:45:19 -0400

    feat: attempt simultaneous cjs and esm module emissions (#350)
    BREAKING CHANGE: changes format of build directory

--- v4-client-js/jest.config.js
+++ v4-client-js/jest.config.js
@@ -1,8 +1,10 @@
 // Use the base configuration as-is.
-exports = require('./node_modules/@dydxprotocol/node-service-base-dev/jest.config');
+exports = require('@dydxprotocol/node-service-base-dev/jest.config');
 
 module.exports = {
   ...exports,
+  roots: ['<rootDir>/build/cjs/__tests__'],
+  testRegex: 'build/cjs/__tests__\\/.*\\.test\\.js$',
   moduleNameMapper: {
     '^axios$': require.resolve('axios'),
   },
