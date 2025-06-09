4ac624b942682777988d62374b7c87dc19318a81
Niloy Datta <niloy.datta@nethermind.io>
2025-06-09 14:51:05 +0600

    Added a method test

--- v4-client-py-v2/tests/indexer/rest/modules/test_vault_endpoints.py
+++ v4-client-py-v2/tests/indexer/rest/modules/test_vault_endpoints.py
@@ -1,12 +1,16 @@
 import pytest
 
 from tests.conftest import retry_on_forbidden
+<<<<<<< HEAD
 
 
+=======
+>>>>>>> e14c3c3 (Added a method test)
 @pytest.mark.asyncio
 @retry_on_forbidden(max_retries=3, delay=1)
 async def test_get_megavault_historical_pnl(indexer_rest_client):
     response = await indexer_rest_client.megavault.get_megavault_historical_pnl("hour")
+<<<<<<< HEAD
     assert response["megavaultPnl"] is not None
 
 
@@ -22,3 +26,6 @@ async def test_get_vault_historical_pnl(indexer_rest_client):
 async def test_get_megavault_positions(indexer_rest_client):
     response = await indexer_rest_client.megavault.get_megavault_positions()
     assert response["positions"] is not None
+=======
+    print(response)
+>>>>>>> e14c3c3 (Added a method test)
