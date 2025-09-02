1a43fc6ae57f2cfb2070d477b7b1607a3d726a4e
Niloy Datta <niloy.datta@nethermind.io>
2025-09-02 14:47:41 +0600

    Added example with builder code parameter only

--- v4-client-py-v2/examples/market_order_example.py
+++ v4-client-py-v2/examples/market_order_example.py
@@ -40,7 +40,7 @@ async def place_market_order(size: float):
         reduce_only=False,
         good_til_block=current_block + 10,
         builder_address=TEST_ADDRESS,
-        fee_ppm=5,
+        fee_ppm=500,
         twap_duration=1000,
         twap_interval=5,
         twap_price_tolerance=5,
@@ -56,4 +56,42 @@ async def place_market_order(size: float):
     wallet.sequence += 1
 
 
+async def place_market_order_with_builder_code(size: float):
+    node = await NodeClient.connect(TESTNET.node)
+    indexer = IndexerClient(TESTNET.rest_indexer)
+
+    market = Market(
+        (await indexer.markets.get_perpetual_markets(MARKET_ID))["markets"][MARKET_ID]
+    )
+    wallet = await Wallet.from_mnemonic(node, DYDX_TEST_MNEMONIC, TEST_ADDRESS)
+
+    order_id = market.order_id(
+        TEST_ADDRESS, 0, random.randint(0, MAX_CLIENT_ID), OrderFlags.SHORT_TERM
+    )
+
+    current_block = await node.latest_block_height()
+
+    new_order = market.order(
+        order_id=order_id,
+        order_type=OrderType.MARKET,
+        side=Order.Side.SIDE_SELL,
+        size=size,
+        price=0,  # Recommend set to oracle price - 5% or lower for SELL, oracle price + 5% for BUY
+        time_in_force=Order.TimeInForce.TIME_IN_FORCE_UNSPECIFIED,
+        reduce_only=False,
+        good_til_block=current_block + 10,
+        builder_address=TEST_ADDRESS,
+        fee_ppm=500,
+    )
+
+    transaction = await node.place_order(
+        wallet=wallet,
+        order=new_order,
+    )
+
+    print(transaction)
+    wallet.sequence += 1
+
+
 asyncio.run(place_market_order(0.00001))
+asyncio.run(place_market_order_with_builder_code(0.00001))
