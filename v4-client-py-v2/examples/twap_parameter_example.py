9173d9d0185ba96ab3af7a31fda9dd826eb3895f
ndatta-nethermind <niloy.datta@nethermind.io>
2025-10-01 18:45:14 +0600

    Ndatta/issue 438 twap param (#441)

---
+++ v4-client-py-v2/examples/twap_parameter_example.py
@@ -0,0 +1,63 @@
+import asyncio
+import random
+
+from dydx_v4_client import MAX_CLIENT_ID, OrderFlags
+from v4_proto.dydxprotocol.clob.order_pb2 import Order
+
+from dydx_v4_client.indexer.rest.constants import OrderType
+from dydx_v4_client.indexer.rest.indexer_client import IndexerClient
+from dydx_v4_client.network import TESTNET
+from dydx_v4_client.node.client import NodeClient
+from dydx_v4_client.node.market import Market
+from dydx_v4_client.wallet import Wallet
+from tests.conftest import DYDX_TEST_MNEMONIC, TEST_ADDRESS
+
+MARKET_ID = "ETH-USD"
+
+
+async def place_market_order_with_twap_parameter(size: float):
+    node = await NodeClient.connect(TESTNET.node)
+    indexer = IndexerClient(TESTNET.rest_indexer)
+
+    market = Market(
+        (await indexer.markets.get_perpetual_markets(MARKET_ID))["markets"][MARKET_ID]
+    )
+    wallet = await Wallet.from_mnemonic(node, DYDX_TEST_MNEMONIC, TEST_ADDRESS)
+
+    order_id = market.order_id(
+        TEST_ADDRESS, 0, random.randint(0, MAX_CLIENT_ID), OrderFlags.SHORT_TERM
+    )
+
+    current_block = await node.latest_block_height()
+
+    new_order = market.order(
+        order_id=order_id,
+        order_type=OrderType.MARKET,
+        side=Order.Side.SIDE_SELL,
+        size=size,
+        price=0,  # Recommend set to oracle price - 5% or lower for SELL, oracle price + 5% for BUY
+        time_in_force=Order.TimeInForce.TIME_IN_FORCE_UNSPECIFIED,
+        reduce_only=False,
+        good_til_block=current_block + 10,
+        twap_duration=7,
+        twap_interval=1,
+        twap_price_tolerance=10,
+    )
+
+    transaction = await node.place_order(
+        wallet=wallet,
+        order=new_order,
+    )
+
+    print(transaction)
+    wallet.sequence += 1
+
+    await asyncio.sleep(5)
+
+    fills = await indexer.account.get_subaccount_fills(
+        address=TEST_ADDRESS, subaccount_number=0, ticker=MARKET_ID, limit=1
+    )
+    print(f"Fills: {fills}")
+
+
+asyncio.run(place_market_order_with_twap_parameter(0.00001))
